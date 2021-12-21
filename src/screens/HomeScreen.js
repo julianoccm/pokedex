0f366ea533b4e3a297135a5c916e3e5c107d97ef
JulianoCCMoreira <julianocolerecmoreira@gmail.com>
2021-12-21 14:13:22 -0300

    Criada a implementacao para o react navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+class HomeScreen extends Component {
+  render() {
+    return <Text>HomeScreen</Text>;
+  }
+}
+
+export default HomeScreen;
