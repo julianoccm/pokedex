5e10859c6ad76722e2e37121772efd825eb49d99
JulianoCCMoreira <julianocolerecmoreira@gmail.com>
2021-12-21 19:39:51 -0300

    Botao trocar pagina, e tela home basica

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,25 @@
-import React from "react";
-import { Text, View, Image, FlatList } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, FlatList, TouchableOpacity } from "react-native";
+
+import { getPokemons } from "../api/PokeAPI";
+
+let PAGE = 12;
 
 const HomeScreen = ({ route }) => {
-  const { pokemonsList } = route.params;
+  const [pokemonsList, setPokemonsList] = useState(route.params.pokemonsList);
 
   return (
-    <View>
+    <View style={{ marginBottom: 80 }}>
+      <TouchableOpacity
+        style={{ backgroundColor: "#8259e3", padding: 10, margin: 8 }}
+        onPress={() => {
+          getPokemons(PAGE).then((data) => setPokemonsList(data));
+          PAGE += 12;
+        }}
+      >
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Proximo</Text>
+      </TouchableOpacity>
+
       <FlatList
         data={pokemonsList}
         keyExtractor={(pokemon) => pokemon.id}
@@ -13,11 +27,9 @@ const HomeScreen = ({ route }) => {
           const spriteUrl = item.sprite + item.id + ".png";
 
           return (
-            <View>
-              <Text>{item.sprite}</Text>
+            <View style={{ borderWidth: 2, margin: 2, padding: 5 }}>
               <Text>{item.id}</Text>
               <Text>{item.nome}</Text>
-              <Text>{item.url}</Text>
               <Image
                 source={{ uri: spriteUrl }}
                 style={{ width: 50, height: 50 }}
