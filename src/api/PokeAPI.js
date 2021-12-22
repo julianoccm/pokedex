1dae0d7aa2ba2940ba6419a2db98292d9e68a2ba
JulianoCCMoreira <julianocolerecmoreira@gmail.com>
2021-12-22 00:08:09 -0300

    Sistema de pesquisa

--- src/api/PokeAPI.js
+++ src/api/PokeAPI.js
@@ -12,21 +12,38 @@ const getPokemons = (initial, limit = 12) => {
     },
   }).then((response) => {
     return response.data.results.map((element) => {
+      const id = element.url
+        .replace("https://pokeapi.co/api/v2/pokemon/", "")
+        .replace("/", "");
+
       return {
-        id: element.url
-          .replace("https://pokeapi.co/api/v2/pokemon/", "")
-          .replace("/", ""),
+        id,
         nome: element.name,
         url: element.url,
-        sprite:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/",
+        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
       };
     });
   });
 };
 
+const getPokemon = (pokemonName) => {
+  return PokeAPI.get(pokemonName)
+    .then((response) => {
+      const pokemon = response.data;
+  
+      return {
+        id: pokemon.id,
+        nome: pokemon.forms[0].name,
+        url: "https://pokeapi.co/api/v2/pokemon/" + pokemon.forms[0].name,
+        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`,
+      };
+    })
+    .catch((error) => {});
+};
+
 module.exports = {
   getPokemons,
+  getPokemon,
 };
 
 export default PokeAPI;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
-import { Text, View, Image, FlatList, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
-import { getPokemons } from "../api/PokeAPI";
+import { getPokemons, getPokemon } from "../api/PokeAPI";
 
 let PAGE = 12;
 
 const HomeScreen = ({ route }) => {
+  const [search, setSearch] = useState("");
+  const [goSearch, setGoSearch] = useState(false);
   const [pokemonsList, setPokemonsList] = useState(route.params.pokemonsList);
 
+  useEffect(() => {
+    if (search !== "") {
+      getPokemon(search).then((data) => setPokemonsList([data]));
+      setGoSearch(false);
+    }
+  }, [goSearch]);
+
   return (
-    <View style={{ marginBottom: 80 }}>
+    <View style={{ marginBottom: 140 }}>
       <TouchableOpacity
         style={{ backgroundColor: "#8259e3", padding: 10, margin: 8 }}
         onPress={() => {
@@ -20,18 +36,24 @@ const HomeScreen = ({ route }) => {
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>Proximo</Text>
       </TouchableOpacity>
 
+      <TextInput
+        value={search}
+        onChangeText={setSearch}
+        onEndEditing={() => setGoSearch(true)}
+        placeholder="Digite o nome do pokemon"
+        style={{ backgroundColor: "grey", margin: 8, padding: 10 }}
+      />
+
       <FlatList
         data={pokemonsList}
         keyExtractor={(pokemon) => pokemon.id}
         renderItem={({ item }) => {
-          const spriteUrl = item.sprite + item.id + ".png";
-
           return (
-            <View style={{ borderWidth: 2, margin: 2, padding: 5 }}>
+            <View style={{ borderWidth: 2, margin: 8, padding: 5 }}>
               <Text>{item.id}</Text>
               <Text>{item.nome}</Text>
               <Image
-                source={{ uri: spriteUrl }}
+                source={{ uri: item.sprite }}
                 style={{ width: 50, height: 50 }}
               />
             </View>
